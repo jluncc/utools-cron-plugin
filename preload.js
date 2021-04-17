cc1238c87a8d13a01b573dbdd5ec087379e93614
jluncc <cjljinglun@163.com>
2021-04-17 22:48:03 +0800

    显示读取输入

--- preload.js
+++ preload.js
@@ -1,7 +1,21 @@
-window.utools.onPluginReady(() => {
-	console.log("插件已准备好");
-	
-	utools.setSubInput(({text}) => {
-		console.log(text);
-	});
-});
+// 分隔cron表达式，判断表达式是否符合要求
+window.checkCron = function(cronExpre) {
+	// console.log("输入的cron: ", cronExpre);
+	cronArray = cronExpre.split(" ");
+	const arrayLen = cronArray.length;
+	// console.log("转换的cronArray:", cronArray, "，长度为:", arrayLen);
+
+	if (arrayLen != 6) {
+		console.log("[格式非法]输入的cron: ", cronExpre);
+		return ["输入的 cron 不符合要求"];
+	} else {
+		console.log("[格式合法]输入的cron: ", cronExpre);
+		return window.analyseCron(cronArray);
+	}
+};
+
+// TODO 根据cron表达式，计算接下来的执行时间
+window.analyseCron = function(cronArray) {
+	console.log("cronArray:", cronArray);
+	return ["输入的 cron 不符合要求"];
+}
